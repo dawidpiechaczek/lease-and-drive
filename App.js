f49ccf470c56964840696692285ef8094c749f98
Dawid Piechaczek <dawidpiechaczek.20@gmail.com>
2021-01-11 18:18:15 +0100

    L&D-1: Import and initial implementation of react navigation stack.

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
 import {
   SafeAreaView,
@@ -13,46 +15,59 @@ import {
   Colors,
   DebugInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function InitScreen() {
+  return (
+    <View style={styles.body}>
+    
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Step One</Text>
+        <Text style={styles.sectionDescription}>
+          Edit <Text style={styles.highlight}>App.js</Text> to change this
+      screen and then come back to see your edits.
+    </Text>
+      </View>
+
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Debug</Text>
+        <Text style={styles.sectionDescription}>
+          <DebugInstructions />
+        </Text>
+      </View>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Learn More</Text>
+        <Text style={styles.sectionDescription}>
+          Read the docs to discover what to do next:
+    </Text>
+      </View>
+      <LearnMoreLinks />
+    </View>
+  )
+}
+
+const Stack = createStackNavigator();
 
 const App: () => React$Node = () => {
   return (
     <>
-      <StatusBar />
-      <SafeAreaView >
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-      
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
+   
+          <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+              <Stack.Screen name="Home" component={HomeScreen} options={{title: "Home Screen"}}/>
+              <Stack.Screen name="Init" component={InitScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+    
 
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
     </>
   );
 };
